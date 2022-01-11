1f29573f87fb9f3455c232904e525d9a8fda1891
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2022-01-11 22:22:55 +0100

    Creating components and specifically bootstrap styling the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './components/recipeBook/recipe-details/recipe-details.component';
+import { RecipesComponent } from './components/recipeBook/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
