e135c1bab8e9368b937af4fd3fb045953ab59601
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2022-01-12 20:25:10 +0100

    Recipe-list component (with dummy recipes)

--- src/app/components/recipeBook/recipe-list/recipe-list.component.ts
+++ src/app/components/recipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe 1', 
+    'This is a test recipe to see how it works', 
+    'https://images.immediate.co.uk/production/volatile/sites/30/2021/08/Sausage-and-mushroom-ragu-203c7d4.jpg'  
+    ),
+    new Recipe('A Test Recipe 2', 
+    'This is a test recipe to see how it works', 
+    'https://images.immediate.co.uk/production/volatile/sites/30/2021/08/Sausage-and-mushroom-ragu-203c7d4.jpg'  
+    ),
+    new Recipe('A Test Recipe 3', 
+    'This is a test recipe to see how it works', 
+    'https://images.immediate.co.uk/production/volatile/sites/30/2021/08/Sausage-and-mushroom-ragu-203c7d4.jpg'  
+    )
+  ];
 
   constructor() { }
 
